810f5e3c342ca81d016fd1a0adbfb58dfe75b947
t3rmin41 <t33rmin41@gmail.com>
2015-02-18 20:25:47 +0200

    Dummy tests added and pom's fixed

---
+++ playground-dao/src/test/java/com/some/swedbank/client/entity/test/KidDaoTest.java
@@ -0,0 +1,12 @@
+package com.some.swedbank.client.entity.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class KidDaoTest {
+	@Test
+	public void mappingKidDaoOK() {
+		assertTrue(true); //dummy
+	}
+}
---
+++ playground-service/src/test/java/com/some/swedbank/client/service/mapper/test/KidDomainMapperTest.java
@@ -0,0 +1,12 @@
+package com.some.swedbank.client.service.mapper.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KidDomainMapperTest {
+	@Test
+	public void mappingKidOK() {
+		assertTrue(true); //dummy
+	}
+}
